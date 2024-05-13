fa4c15604530bc36771dfd81b7e12f0661640d36
myavzur <shwave_id404@mail.ru>
2024-05-13 14:54:16 +0300

    Router config

--- src/app/providers/AppRouterProvider/AppRouterProvider.tsx
+++ src/app/providers/AppRouterProvider/AppRouterProvider.tsx
@@ -1,49 +1,11 @@
 import React, { Suspense } from "react";
-import {
-	BrowserRouter,
-	Navigate,
-	NavigateFunction,
-	Route,
-	Routes,
-	useNavigate
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-// import { AuthLayout } from "@/layouts/auth-layout/ui";
-// import { ChatsLayout } from "@/layouts/chats-layout/ui";
+import { ProtectedRoute } from "./ProtectedRoute";
 
-// import { useAuth } from "@/entities/user/lib/hooks";
-
-// import { PageLoader } from "@/shared/ui";
-// const Chats = React.lazy(() => import("@/screens/chats"));
-// const Chat = React.lazy(() => import("@/screens/chat"));
 const LoginScreen = React.lazy(() => import("@/screens/auth/login"));
 const RegisterScreen = React.lazy(() => import("@/screens/auth/register"));
 
-export const History: {
-	navigate: (path: string) => void | NavigateFunction;
-} = {
-	navigate: () => {
-		return;
-	}
-};
-
-const NavigateSetter = () => {
-	History.navigate = useNavigate();
-	return null;
-};
-
-const ProtectedRoute: React.FC<{
-	redirectPath: string;
-	hasAccess: boolean;
-	outlet: JSX.Element;
-}> = ({ hasAccess, outlet, redirectPath }) => {
-	return hasAccess ? outlet : <Navigate to={redirectPath} />;
-};
-
-/** Uses PageLoader for 2 cases.
- * * Fetching user data from server
- * * Loading lazy page
- */
 export const AppRouterProvider: React.FC = () => {
 	// const { isAuthorized, isCurrentUserFetching } = useAuth();
 
@@ -53,8 +15,6 @@ export const AppRouterProvider: React.FC = () => {
 
 	return (
 		<BrowserRouter>
-			<NavigateSetter />
-
 			<Suspense fallback={<h1>Loading now...</h1>}>
 				<Routes>
 					<Route
@@ -63,7 +23,7 @@ export const AppRouterProvider: React.FC = () => {
 							<ProtectedRoute
 								redirectPath="/auth/login"
 								hasAccess={false}
-								outlet={<h1>Чаты</h1>}
+								element={<h1>Чаты</h1>}
 							/>
 						}
 					>
