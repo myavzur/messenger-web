59cd834e82d68afa2ad10b774411147219c5ea3a
myavzur <shwave_id404@mail.ru>
2024-06-24 16:00:41 +0300

    Jest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,27 @@
-import * as path from "path";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 import pluginChecker from "vite-plugin-checker";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@/": path.resolve(__dirname, "src"),
-      "@/app": path.resolve(__dirname, "src/app"),
-      "@/processes": path.resolve(__dirname, "src/processes"),
-      "@/screens": path.resolve(__dirname, "src/screens"),
-      "@/layouts": path.resolve(__dirname, "src/layouts"),
-      "@/widgets": path.resolve(__dirname, "src/widgets"),
-      "@/features": path.resolve(__dirname, "src/features"),
-      "@/entities": path.resolve(__dirname, "src/entities"),
-      "@/shared": path.resolve(__dirname, "src/shared"),
-    },
-  },
-  plugins: [react(), pluginChecker({ typescript: true })],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/setup-tests.ts"
+	},
+	resolve: {
+		alias: {
+			"@/": path.resolve(__dirname, "src"),
+			"@/app": path.resolve(__dirname, "src/app"),
+			"@/processes": path.resolve(__dirname, "src/processes"),
+			"@/screens": path.resolve(__dirname, "src/screens"),
+			"@/layouts": path.resolve(__dirname, "src/layouts"),
+			"@/widgets": path.resolve(__dirname, "src/widgets"),
+			"@/features": path.resolve(__dirname, "src/features"),
+			"@/entities": path.resolve(__dirname, "src/entities"),
+			"@/shared": path.resolve(__dirname, "src/shared")
+		}
+	},
+	plugins: [react(), pluginChecker({ typescript: true })]
 });
